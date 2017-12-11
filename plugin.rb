86da8e59bc9c8a8bb53bb9cf35ba703eed11feb0
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2017-12-11 12:19:03 +0530

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: preventing-malicious-linking-plugin
+# about: This is intended to be a plugin for preventing malicious liking in discourse forums.
+# version: 0.1
+# authors: Sudaraka Jayathilaka
+# url: https://github.com/sudaraka94/preventing-malicious-linking-plugin.git
+
