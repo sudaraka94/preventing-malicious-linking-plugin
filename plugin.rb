4bcc429c1afb4d6e02c7ea3efea6639e647128f9
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2017-12-12 23:03:18 +0530

    Identifies all urls in a post with regex

--- plugin.rb
+++ plugin.rb
@@ -4,3 +4,29 @@
 # authors: Sudaraka Jayathilaka
 # url: https://github.com/sudaraka94/preventing-malicious-linking-plugin.git
 
+require 'uri'
+
+# Returns an array of urls in the given string using regex
+def getUrls(post_body)
+  urls=Array[]
+  words = post_body.split
+  words.each do |word|
+    if(/\S+\.\S+/.match(word))
+      urls.push(word)
+    end
+  end
+  return urls
+end
+
+# Returns malicious urls in a string
+def getMalicioudUrls(urls)
+
+end
+
+DiscourseEvent.on(:post_created) do |post|
+  urls=getUrls(post.raw)
+
+  puts 'initialized'
+  puts urls
+end
+
