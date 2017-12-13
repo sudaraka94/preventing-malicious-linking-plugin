91fadd2a4f1397206a9fd8025d2c84a56783a667
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2017-12-13 09:30:46 +0530

    Identifies all the threats in all the urls in the post

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,8 @@
 # url: https://github.com/sudaraka94/preventing-malicious-linking-plugin.git
 
 require 'uri'
+require 'faraday'
+require 'json'
 
 # Returns an array of urls in the given string using regex
 def getUrls(post_body)
@@ -21,12 +23,36 @@ end
 # Returns malicious urls in a string
 def getMalicioudUrls(urls)
 
+  url_str=''
+  # Process urls
+  urls.each do |url|
+    if(url_str=='')
+      url_str=url_str+"{\"url\": \"#{url}\"}"
+    else
+      url_str=url_str+",{\"url\": \"#{url}\"}"
+    end
+  end
+  req_body="{\"client\": {\"clientId\":\"yourcompanyname\",\"clientVersion\": \"0.1\"},\"threatInfo\":{\"threatTypes\":[\"MALWARE\", \"SOCIAL_ENGINEERING\"],\"platformTypes\":[\"ANY_PLATFORM\"],\"threatEntryTypes\": [\"URL\"],\"threatEntries\": [#{url_str}]}}"
+  puts req_body
+  response = Faraday.post do |req|
+    req.url "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=<api key>"
+    req.headers['Content-Type'] = 'application/json'
+    req.body = req_body
+  end
+
+  parsed_response=JSON.parse response.body
+  threats=parsed_response['matches']
+  if(threats.count()==1)
+    puts "One threat found !"
+  else
+    puts threats.count().to_s+" threats found !"
+  end
 end
 
 DiscourseEvent.on(:post_created) do |post|
   urls=getUrls(post.raw)
-
   puts 'initialized'
+  getMalicioudUrls(urls)
   puts urls
 end
 
